42a4e4840084552469d26956b11db92ddcc7a646
Ivan Spajić <ivan.spajic@gmail.com>
2025-03-01 20:17:03 +0100

    Successfully got a trivial custom inference rule to work.

--- Inference Rules/inference_rules/src/main/java/cps_code_generation/inference_rules/App.java
+++ Inference Rules/inference_rules/src/main/java/cps_code_generation/inference_rules/App.java
@@ -19,16 +19,20 @@ public class App
 	
     public static void main( String[] args ) throws FileNotFoundException
     {
+    	// Load the ontology (meta-model) and instance, and set up an OWL reasoner to use the ontology as a
+    	// schema for the instance model.
     	Model ontology = RDFDataMgr.loadModel(ontologyBasePath + "cps-code-generation.ttl");
     	Model instanceModel = RDFDataMgr.loadModel(instanceBasePath + "instance-model-1.ttl");
     	Reasoner owlReasoner = ReasonerRegistry.getOWLReasoner()
     			.bindSchema(ontology);
     	
+    	// Infer the preliminary inferred model from basic OWL rules.
     	InfModel basicInferredModel = ModelFactory.createInfModel(owlReasoner, instanceModel);
     	
+    	// Load the list of custom inference rules, instantiate a generic rule reasoner, and infer the final
+    	// model.
     	List ruleList = Rule.rulesFromURL(instanceBasePath + "instance-model-1-inference-rules.rules");
     	GenericRuleReasoner ruleReasoner = new GenericRuleReasoner(ruleList);
-    	
     	InfModel finalInferredModel = ModelFactory.createInfModel(ruleReasoner, basicInferredModel);
     	
     	FileOutputStream fileOutputStream = new FileOutputStream(instanceBasePath + "inferred-model-1.ttl");

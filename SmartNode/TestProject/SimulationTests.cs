7a3d926d6b0e1ce3a58cd2a4117cad38297471c1
Ivan Spajic <ispa@hvl.no>
2025-08-21 15:26:32 +0200

    Updated diagrams for the paper and added a unit test for loading and executing FMUs.

---
+++ SmartNode/TestProject/SimulationTests.cs
@@ -0,0 +1,38 @@
+﻿using Femyou;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class SimulationTests
+    {
+        [Fact]
+        public void Simulation_loads_and_executes_Fmu()
+        {
+            // Arrange
+            var modelName = "../../../../SensorActuatorImplementations/FMUs/roomM370.fmu";
+            var expectedValue = 1.0;
+            var actualValue = 0.0;
+
+            // Act
+            var model = Model.Load(modelName);
+            var energyConsumption = model.Variables["EnergyConsumption"];
+
+            var fmuInstance = model.CreateCoSimulationInstance("demo");
+
+            fmuInstance.StartTime(0);
+            fmuInstance.WriteReal((energyConsumption, expectedValue));
+            fmuInstance.AdvanceTime(10);
+            actualValue = fmuInstance.ReadReal(energyConsumption).ToArray()[0];
+
+            fmuInstance.Dispose();
+            model.Dispose();
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}

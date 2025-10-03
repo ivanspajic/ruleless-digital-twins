cd381c2eec3f1b45bd493033ba63ee400e9ddb81
Ivan Spajić <ivan.spajic@gmail.com>
2025-10-03 15:40:05 +0200

    Added a test for FMU time discrepancies.

--- SmartNode/TestProject/SimulationTests.cs
+++ SmartNode/TestProject/SimulationTests.cs
@@ -38,5 +38,43 @@ namespace TestProject
             // Assert
             Assert.Equal(expectedValue, actualValue);
         }
+
+        [Theory]
+        [InlineData(900)]
+        public void Fmu_returns_same_value_after_specific_simulation_time(int simulationTimeSeconds)
+        {
+            // Arrange
+            // Use a dummy-type to get a handle:
+            var assembly = Assembly.GetAssembly(typeof(SensorActuatorImplementations.ValueHandlers.DoubleValueHandler));
+            Stream modelStream = assembly.GetManifestResourceStream("SensorActuatorImplementations.FMUs.roomM370.fmu");
+            Assert.NotNull(modelStream);
+
+            var roomTemperatureInitialValue = 1.02;
+            var acUnitStateValue = 1;
+            var expectedValue = 11.0965;
+            var actualValue = 0.0;
+
+            // Act
+            var model = Model.Load(modelStream, "roomM370.fmu");
+            var fmuInstance = model.CreateCoSimulationInstance("demo");
+
+            var roomTemperature = model.Variables["RoomTemperature"];
+            var acUnitState = model.Variables["AirConditioningUnitState"];
+
+            fmuInstance.StartTime(0);
+
+            fmuInstance.WriteReal((roomTemperature, roomTemperatureInitialValue));
+            fmuInstance.WriteInteger((acUnitState, acUnitStateValue));
+
+            fmuInstance.AdvanceTime(simulationTimeSeconds);
+
+            actualValue = fmuInstance.ReadReal(roomTemperature).ToArray()[0];
+
+            // fmuInstance.Dispose();
+            // model.Dispose();
+
+            // Assert
+            Assert.Equal(expectedValue, actualValue);
+        }
     }
 }

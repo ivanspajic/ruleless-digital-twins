c47ecfccf5adff274a3d7327875cb485812940a3
Volker Stolz <vsto@hvl.no>
2025-11-26 22:58:55 +0000

    Tidy code, add methods to tweak mocked heater & lid (#34).

---
+++ SmartNode/Implementations/Sensors/IncubatorAdapter.cs
@@ -0,0 +1,96 @@
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Implementations.Sensors {
+
+    // Case-sensitive!
+    public record class IncubatorFields(double t1, double t2, double t3, double average_temperature, double execution_interval, double elapsed,
+                                ulong time_t1, ulong time_t2, ulong time_t3,
+                                bool heater_on, bool fan_on);
+    public record class Incubator(string measurement, IncubatorFields fields);
+
+    public class IncubatorAdapter {
+        private readonly ConnectionFactory _factory;
+        private readonly CancellationToken _ct;
+        public IncubatorFields? Data;
+        public ulong Counter = 0;
+
+        readonly string ExchangeName = "Incubator_AMQP"; // From Incubator
+        readonly double G_box = 0.5763498; // startup.conf
+
+        public IConnection? Conn { get; private set; }
+        public IChannel? Channel { get; private set; }
+
+        public IncubatorAdapter(string hostName, CancellationToken ct) {
+            _factory = new() {
+                UserName = "incubator",
+                Password = "incubator",
+                HostName = hostName
+            };
+            _ct = ct;
+        }
+
+        public async Task Connect()
+        {
+            Conn = await _factory.CreateConnectionAsync(cancellationToken: _ct);            
+            Channel = await Conn.CreateChannelAsync(cancellationToken: _ct);
+            await Channel.ExchangeDeclareAsync(ExchangeName, ExchangeType.Topic, cancellationToken: _ct);
+        }
+
+        public async Task<string> Setup() {
+            if (Channel == null) {
+                throw new Exception();
+            }
+            var queueName = "mine_local"; // Under our control
+            await Channel.QueueDeclareAsync(queueName, false, false, false, null, cancellationToken: _ct);
+            // From Incubator: incubator_state_csv_recorder.py:
+            await Channel.QueueBindAsync(queueName, ExchangeName, "incubator.record.driver.state", null, cancellationToken: _ct);
+
+            var _lock = this;
+
+            var consumer = new AsyncEventingBasicConsumer(Channel);
+            consumer.ReceivedAsync += async (ch, ea) => {
+                var body = ea.Body.ToArray();
+                await Channel.BasicAckAsync(ea.DeliveryTag, false);
+                string json = Encoding.Default.GetString(body);
+                var fromJson = JsonSerializer.Deserialize<Incubator>(json);
+                Debug.Assert(fromJson != null);
+                // I guess the fun starts when async events start overtaking each other here.
+                // TODO: Use a proper queue.
+                Monitor.Enter(_lock);
+                Data = fromJson.fields;
+                Counter++;
+                Monitor.Exit(_lock);
+            };
+            return await Channel.BasicConsumeAsync(queueName, false, consumer, cancellationToken: _ct);
+        }
+
+        record class GBox(double G_box);
+
+        // software/cli/mess_with_lid_mock.py
+        public async Task SendGBoxConfig(double g) {
+            if (Channel == null) {
+                throw new Exception();
+            }
+            var new_g_box = new GBox(g * G_box);
+            // byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"{{\"G_box\": {new_g_box}}}"); // Arrrrr
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize<GBox>(new_g_box)); // Arrrrr
+            var props = new BasicProperties();
+            await Channel.BasicPublishAsync(ExchangeName, "incubator.mock.hw.box.G", false, props, messageBodyBytes);
+        }
+
+        record class Heater(Boolean heater);
+        public async Task SetHeater(Boolean on) {
+            if (Channel == null) {
+                throw new Exception();
+            }
+            var new_heater = new Heater(on);
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(JsonSerializer.Serialize<Heater>(new_heater)); // Arrrrr
+            var props = new BasicProperties();
+            await Channel.BasicPublishAsync(ExchangeName, "incubator.mock.hw.heater.on", false, props, messageBodyBytes);
+        }
+    }
+}
--- SmartNode/TestProject/IncubatorAMQPTest.cs
+++ SmartNode/TestProject/IncubatorAMQPTest.cs
@@ -1,47 +1,45 @@
-using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using System.Diagnostics;
+using Implementations.Sensors;
 
 namespace TestProject {
 
-public class IncubatorAMQPTest
-    {
+    // NB: First time you might see a single wild {c} value in the 20s. Rerunning then gives you around 5.
+    public class IncubatorAMQPTest {
         [Theory]
         [InlineData("192.168.64.1")] // Should probably come from the outside since it depends where your Incubator-container is running.
         public async Task TestConnect(string hostName) {
-            ConnectionFactory factory = new() {
-                UserName = "incubator",
-                Password = "incubator",
-                HostName = hostName
-            };
-
-            IConnection conn = await factory.CreateConnectionAsync(cancellationToken: TestContext.Current.CancellationToken);
-            IChannel channel = await conn.CreateChannelAsync(cancellationToken: TestContext.Current.CancellationToken);
-            var exchangeName = "Incubator_AMQP"; // From Incubator
-            var queueName = "mine_local"; // Under our control
-            var routingKey = "incubator.record.driver.state"; // From Incubator: incubator_state_csv_recorder.py
-            await channel.ExchangeDeclareAsync(exchangeName, ExchangeType.Topic, cancellationToken: TestContext.Current.CancellationToken);
-            await channel.QueueDeclareAsync(queueName, false, false, false, null, cancellationToken: TestContext.Current.CancellationToken);
-            await channel.QueueBindAsync(queueName, exchangeName, routingKey, null, cancellationToken: TestContext.Current.CancellationToken);
-
-            var tcs = new TaskCompletionSource<object>();
-            var consumer = new AsyncEventingBasicConsumer(channel);
-            var counter = 3;
-            consumer.ReceivedAsync += async (ch, ea) => {
-                                var body = ea.Body.ToArray();
-                                await channel.BasicAckAsync(ea.DeliveryTag, false);
-                                // For the Incubator it's JSON:
-                                // Nope out after three messages:
-                                if (counter-- <= 0) {                                    
-                                    tcs.SetResult(null);
-                                } else { // eat reamining messages so that they don't spam the console.
-                                    System.Diagnostics.Trace.WriteLine(Encoding.Default.GetString(body));
-                                }
-                            };
-            string consumerTag = await channel.BasicConsumeAsync(queueName, false, consumer, cancellationToken: TestContext.Current.CancellationToken);
-            System.Diagnostics.Trace.WriteLine("Waiting. Tag: " + consumerTag);
-            await tcs.Task;
-            System.Diagnostics.Trace.WriteLine("Done.");
+            var i = new IncubatorAdapter(hostName, TestContext.Current.CancellationToken);
+            await i.Connect();
+            var consumerTag = await i.Setup();
+            IncubatorFields? myData = null;
+            bool seenFirstData = false; // Safety net. Should be monotone, and myData never turn null again (unless JSON fails).
+            ulong c = 0;
+            while (c < 25) {
+                // Incubator by default ticks every 3 seconds, so we may see some repetitions here:
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+                Monitor.Enter(i);
+                c = i.Counter;
+                myData = i.Data;
+                Monitor.Exit(i);
+                if (myData != null || seenFirstData) {
+                    Assert.NotNull(myData);
+                    // TODO: format human readable time.
+                    Trace.WriteLine($"Observed ({c}): {myData.average_temperature} @ {myData.time_t1}");
+                    seenFirstData = true;
+                }
+                if (c == 10) {
+                    Trace.WriteLine("Opening the lid...");
+                    await i.SendGBoxConfig(100);
+                }
+                if (c == 8) {
+                    Trace.WriteLine("Micromanaging heater...");
+                    await i.SetHeater(!myData.heater_on);
+                }
+                if (c == 20) {
+                    Trace.WriteLine("Closing the lid...");
+                    await i.SendGBoxConfig(1);
+                }
+            }
         }
     }
 }

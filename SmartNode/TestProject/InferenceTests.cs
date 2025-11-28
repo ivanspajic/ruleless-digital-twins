dee9d22745caf2c6f0df418b8542c873179dff89
Ivan Spajic <ispa@hvl.no>
2025-11-28 16:46:23 +0100

    Added tests, test data, and test files for inference tests.

--- SmartNode/TestProject/InferenceTests.cs
+++ SmartNode/TestProject/InferenceTests.cs
@@ -1,13 +1,88 @@
-﻿namespace TestProject
+﻿using Logic.Mapek;
+using Logic.Models.OntologicalModels;
+using System.Diagnostics;
+using System.Reflection;
+using TestProject.Utilities;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+using VDS.RDF.Query;
+
+namespace TestProject
 {
     public class InferenceTests
     {
-        private const IEnumerable<Action>
-
         [Theory]
-        [InlineData()]
-        public void Correct_action_combinations_for_instance_model(string filepath, ) {
-            
+        [MemberData(nameof(InferenceTestHelper.TestData), MemberType = typeof(InferenceTestHelper))]
+        public void Correct_action_combinations_for_instance_model(string instanceModelFilepath, IEnumerable<IEnumerable<ActuationAction>> combinations) {
+            // Arrange
+            var executingAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var workingDirectoryPath = $"{executingAssemblyPath}/../../models-and-rules/";
+            var ontologyFilepath = $"{executingAssemblyPath}/../../Ontology/ruleless-digital-twins.ttl";
+            var inferenceRulesFilepath = $"{executingAssemblyPath}/../../models-and-rules/inference-rules.rules";
+            var inferredInstanceModelFilepath = Path.Combine("inferred_", instanceModelFilepath);
+
+            var inferredModel = new Graph();
+            var turtleParser = new TurtleParser();
+
+            // Act
+            ExecuteJarFile("../../../models-and-rules/ruleless-digital-twins-inference-engine.jar",
+                [$"{ontologyFilepath}", $"{instanceModelFilepath}", $"{inferenceRulesFilepath}", $"{inferredInstanceModelFilepath}"],
+                workingDirectoryPath);
+
+            turtleParser.Load(inferredModel, inferredInstanceModelFilepath);
+
+            var actionCombinations = GetActionCombinationsFromInferredModel(inferredModel);
+
+            // Assert
+
+        }
+
+        private static void ExecuteJarFile(string jarFilepath, string[] arguments, string workingDirectoryPath) {
+            var process = new Process();
+
+            process.StartInfo.FileName = "java";
+            process.StartInfo.Arguments = $"-jar {jarFilepath} {string.Join(" ", arguments)}";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WorkingDirectory = workingDirectoryPath;
+
+            process.Start();
+
+            string standardOutput = process.StandardOutput.ReadToEnd();
+            string standardError = process.StandardError.ReadToEnd();
+
+            process.WaitForExit();
+        }
+
+        private static List<List<ActuationAction>> GetActionCombinationsFromInferredModel(IGraph inferredModel) {
+            var actionCombinations = new List<List<ActuationAction>>();
+
+            var actuationQuery = MapekUtilities.GetParameterizedStringQuery();
+
+            actuationQuery.CommandText = @"SELECT ?actionCombination (GROUP_CONCAT(?action; SEPARATOR="" "") AS ?actions) WHERE {
+	                ?actionCombination rdf:type meta:ActionCombination .
+	                FILTER NOT EXISTS {
+		                {
+			                ?actionCombination rdf:comment ""duplicate""^^<http://www.w3.org/2001/XMLSchema#string> .
+		                }
+		                UNION
+		                {
+			                ?actionCombination rdf:comment ""not final""^^<http://www.w3.org/2001/XMLSchema#string> .
+		                }
+	                }
+	                ?actionCombination meta:hasActions ?actionList .
+	                ?actionList rdf:rest*/rdf:first ?action . }
+                GROUP BY ?actionCombination";
+
+            var actuationQueryResult = (SparqlResultSet)inferredModel.ExecuteQuery(actuationQuery);
+
+            foreach (var result in actuationQueryResult.Results) {
+                
+            }
+
+            return actionCombinations;
         }
     }
 }
---
+++ SmartNode/TestProject/Utilities/InferenceTestHelper.cs
@@ -0,0 +1,283 @@
+﻿using Logic.Models.OntologicalModels;
+using System.Diagnostics;
+
+namespace TestProject.Utilities {
+    internal static class InferenceTestHelper {
+        public static TheoryData<string, IEnumerable<IEnumerable<ActuationAction>>> TestData =>
+            new() {
+                {
+                    @"/TestFiles/unrestrictedDehumidifierFanHeater.ttl",
+                    new[] {
+                        new ActuationAction[] {
+                            new() {
+                                Name = "Dehumidifier_0",
+                                Actuator = new Actuator {
+                                    Name = "Dehumidifier"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Dehumidifier_1",
+                                Actuator = new Actuator {
+                                    Name = "Dehumidifier"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "Fan_0",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Fan_1",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue= "1"
+                            },
+                            new() {
+                                Name = "Heater_0",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Heater_1",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "Heater_2",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "2"
+                            }
+                        }
+                    }
+                },
+                {
+                    @"/TestFiles/restrictedDehumidifierFanHeater.ttl",
+                    new[] {
+                        new ActuationAction[] {
+                            new() {
+                                Name = "Fan_0",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Fan_1",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue= "1"
+                            },
+                            new() {
+                                Name = "Heater_0",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Heater_1",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "Heater_2",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "2"
+                            }
+                        }
+                    }
+                },
+                {
+                    @"/TestFiles/unrestrictedDehumidifierFanHeaterFloorHeating.ttl",
+                    new[] {
+                        new ActuationAction[] {
+                            new() {
+                                Name = "Dehumidifier_0",
+                                Actuator = new Actuator {
+                                    Name = "Dehumidifier"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Dehumidifier_1",
+                                Actuator = new Actuator {
+                                    Name = "Dehumidifier"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "Fan_0",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Fan_1",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue= "1"
+                            },
+                            new() {
+                                Name = "Heater_0",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Heater_1",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "FloorHeating_0",
+                                Actuator = new Actuator {
+                                    Name = "FloorHeating"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "FloorHeating_1",
+                                Actuator = new Actuator {
+                                    Name = "FloorHeating"
+                                },
+                                NewStateValue = "1"
+                            }
+                        }
+                    }
+                },
+                {
+                    @"/TestFiles/restrictedDehumidifierFanHeaterFloorHeating.ttl",
+                    new[] {
+                        new ActuationAction[] {
+                            new() {
+                                Name = "Fan_0",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Fan_1",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue= "1"
+                            },
+                            new() {
+                                Name = "Heater_0",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Heater_1",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "FloorHeating_0",
+                                Actuator = new Actuator {
+                                    Name = "FloorHeating"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "FloorHeating_1",
+                                Actuator = new Actuator {
+                                    Name = "FloorHeating"
+                                },
+                                NewStateValue = "1"
+                            }
+                        }
+                    }
+                },
+                {
+                    @"/TestFiles/restrictedDehumidifierFanHeaterManyStates.ttl",
+                    new[] {
+                        new ActuationAction[] {
+                            new() {
+                                Name = "Fan_0",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Fan_1",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue= "1"
+                            },
+                            new() {
+                                Name = "Fan_2",
+                                Actuator = new Actuator {
+                                    Name = "Fan"
+                                },
+                                NewStateValue = "2"
+                            },
+                            new() {
+                                Name = "Heater_0",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "0"
+                            },
+                            new() {
+                                Name = "Heater_1",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "1"
+                            },
+                            new() {
+                                Name = "Heater_2",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "2"
+                            },
+                            new() {
+                                Name = "Heater_3",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "3"
+                            },
+                            new() {
+                                Name = "Heater_4",
+                                Actuator = new Actuator {
+                                    Name = "Heater"
+                                },
+                                NewStateValue = "4"
+                            }
+                        }
+                    }
+                }
+            };
+    }
+}

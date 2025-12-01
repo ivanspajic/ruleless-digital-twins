6823c73b8e2c0823155fdca7318f8be4fa1d19a4
Volker Stolz <vsto@hvl.no>
2025-12-01 13:36:29 +0000

    Initial prototyping around NordPool-FMU (#27)

---
+++ SmartNode/TestProject/NordPoolTests.cs
@@ -0,0 +1,91 @@
+using Logic.Mapek;
+using Logic.Mapek.Comparers;
+using Logic.Models.MapekModels;
+using Logic.Models.OntologicalModels;
+using System.Reflection;
+using TestProject.Mocks;
+using TestProject.Mocks.EqualityComparers;
+using VDS.RDF;
+using VDS.RDF.Parsing;
+
+namespace TestProject
+{
+    public class NordPoolTests
+    {
+        [Fact]
+        public void Actions_and_OptimalConditions_for_plan_phase_same_as_expected()
+        {
+            // Arrange
+            var executingAssemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var modelFilePath = Path.Combine(executingAssemblyPath!, $"..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}..{Path.DirectorySeparatorChar}models-and-rules{Path.DirectorySeparatorChar}nordpool-out.ttl");
+            modelFilePath = Path.GetFullPath(modelFilePath);
+
+            var instanceModel = new Graph();
+            var turtleParser = new TurtleParser();
+            turtleParser.Load(instanceModel, modelFilePath);
+
+            var mapekAnalyze = new MapekAnalyze(new ServiceProviderMock());
+
+            var simulationGranularity = 4;
+
+            var propertyCacheMock = new PropertyCache
+            {
+                ConfigurableParameters = new Dictionary<string, ConfigurableParameter>(),
+                Properties = new Dictionary<string, Property>
+                {
+                    {
+                        "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#MeasuredOutputProperty",
+                        new Property
+                        {
+                            Name = "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#MeasuredOutputProperty",
+                            OwlType = "double",
+                            Value = -1.02
+                        }
+                    },
+                    {
+                        "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#DummyProperty",
+                        new Property
+                        {
+                            Name = "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#DummyProperty",
+                            OwlType = "double",
+                            Value = -1.02
+                        }
+                    },
+                    {
+                        "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#PriceMeasure",
+                        new Property
+                        {
+                            Name = "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#PriceMeasure",
+                            OwlType = "double",
+                            Value = -1.02
+                        }
+                    },
+                    {
+                        "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#price",
+                        new Property
+                        {
+                            Name = "http://www.semanticweb.org/vs/ontologies/2025/11/untitled-ontology-97#price",
+                            OwlType = "double",
+                            Value = -1.02
+                        }
+                    },
+                    {
+                        "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#HumiditySensorMeasuredHumidity",
+                        new Property
+                        {
+                            Name = "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#HumiditySensorMeasuredHumidity",
+                            OwlType = "double",
+                            Value = -1.02
+                        }
+                    }
+                }
+            };
+
+            // Act
+            var actualOptimalConditionActionTuple = mapekAnalyze.Analyze(instanceModel, propertyCacheMock, simulationGranularity);
+            var str = actualOptimalConditionActionTuple.Item1
+                .Aggregate(string.Empty, (current, condition) => current + $"{condition}\n");
+            Assert.True(actualOptimalConditionActionTuple.Item1.Count > 0, str);
+        }
+    }
+}

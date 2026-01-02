a1424a8a9281d9572a7d87b30651eb0bff4e20d2
Volker Stolz <vsto@hvl.no>
2026-01-02 22:57:06 +0000

    Tidy.

--- SmartNode/TestProject/IncubatorTests.cs
+++ SmartNode/TestProject/IncubatorTests.cs
@@ -21,11 +21,7 @@ namespace TestProject
 
         private class MyMapekPlan : MapekPlan {
 
-            public MyMapekPlan(IServiceProvider serviceProvider, bool logSimulations = false) : base(serviceProvider) {
-                // TODO: If the simulation runs overboard and the FMU throws LOG_ASSERT,
-                // FMI calls with fail ungracefully.
-                //MaximumSimulationTimeSeconds = 10;
-            }
+            public MyMapekPlan(IServiceProvider serviceProvider, bool logSimulations = false) : base(serviceProvider) {}
 
             protected override void InferActionCombinations() {
                 // Call Java explicitly?
@@ -37,7 +33,7 @@ namespace TestProject
 
         [Theory]
         [InlineData("Incubator.py", "incubator.ttl", "incubator-out.ttl", 4)]
-        public void Simulate(string? fromPython, string model, string inferred, int lookAheadCycles)
+        public void SimulateFMUOnly(string fromPython, string model, string inferred, int lookAheadCycles)
         {
             SetupFiles(fromPython, model, inferred, out ServiceProviderMock mock, out FilepathArguments filepathArguments, out MapekKnowledge mapekKnowledge, out MyMapekPlan mapekPlan);
 
@@ -120,18 +116,9 @@ namespace TestProject
 
         [Theory]
         [InlineData("Incubator.py", "incubator.ttl", "incubator-out.ttl", 4)]
-        public async Task FetchFromAMQ(string? fromPython, string model, string inferred, int lookAheadCycles)
+        public async Task SimulateFromAMQ(string fromPython, string model, string inferred, int lookAheadCycles)
         {
             SetupFiles(fromPython, model, inferred, out ServiceProviderMock mock, out FilepathArguments filepathArguments, out MapekKnowledge mapekKnowledge, out MyMapekPlan mapekPlan);
-
-            // TODO: Prototype populate cache from FMU.
-            // If we're going to do this, we have to check that we correctly override with values from model.
-            var fmu = Femyou.Model.Load(Path.Combine(filepathArguments.FmuDirectory, "au_incubator.fmu")); // TODO: grab from model
-            var (SvType, SvValue) = fmu.Variables["G_box"]!.StartValue;
-            Assert.Equal("Real", SvType);
-            double gbox = double.Parse(SvValue);
-            fmu.Dispose(); // Don't forget this or you'll get segfaults when loading the FMU "again" later.
-            // END Prototype
 
             await i.Connect();
             var consumerTag = await i.Setup();
@@ -162,21 +149,23 @@ namespace TestProject
             }
         }
 
-        private static void SetupFiles(string? fromPython, string model, string inferred, out ServiceProviderMock mock, out FilepathArguments filepathArguments, out MapekKnowledge mapekKnowledge, out MyMapekPlan mapekPlan)
+        private static void SetupFiles(string fromPython, string model, string inferred, out ServiceProviderMock mock, out FilepathArguments filepathArguments, out MapekKnowledge mapekKnowledge, out MyMapekPlan mapekPlan)
         {
             var rootDirectory = Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.Parent!.Parent!.Parent!.Parent!.Parent!.FullName;
-            var modelFilePath = Path.Combine(rootDirectory, "models-and-rules");
-            var inferredFilePath = Path.Combine(rootDirectory, $"models-and-rules{Path.DirectorySeparatorChar}{inferred}");
+            var modelDirPath = Path.Combine(rootDirectory, "models-and-rules");
+            var inferredFilePath = Path.Combine(modelDirPath, inferred);
+            var modelFilePath = Path.Combine(modelDirPath, model);
+            modelFilePath = Path.GetFullPath(modelFilePath);
             // TODO: Review why file must exist if we're going to overwrite it anyway.
-            if (!File.Exists(inferredFilePath))
-            {
+            if (!File.Exists(inferredFilePath)) {
                 File.Create(inferredFilePath).Close();
+                runInference = true;
+            } else {
+                DateTime x,y;
+                runInference = !((x = File.GetLastWriteTime(inferredFilePath)) > (y = File.GetLastWriteTime(modelFilePath)));
             }
 
-            GenerateFromPython(fromPython, model, rootDirectory, modelFilePath);
-
-            modelFilePath = Path.Combine(rootDirectory, $"models-and-rules{Path.DirectorySeparatorChar}{model}");
-            modelFilePath = Path.GetFullPath(modelFilePath);
+            GenerateFromPython(fromPython, model, rootDirectory);
 
             mock = new ServiceProviderMock(new Factory());
             filepathArguments = new FilepathArguments
@@ -203,31 +192,29 @@ namespace TestProject
             mapekPlan = new MyMapekPlan(mock, false);
         }
 
-        private static void GenerateFromPython(string? fromPython, string model, string executingAssemblyPath, string modelFilePath) {
-            if (fromPython != null) {
-                var outPath = Path.Combine(executingAssemblyPath!, $"models-and-rules{Path.DirectorySeparatorChar}{model}");
-                outPath = Path.GetFullPath(outPath);
-                Assert.True(File.Exists(Path.Combine(modelFilePath, fromPython)));
-                Assert.True(File.Exists(Path.Combine(modelFilePath, "RDTBindings.py")));
-                if (runInference || !File.Exists(outPath) || File.GetLastWriteTime(Path.Combine(modelFilePath, fromPython)) > File.GetLastWriteTime(outPath) || File.GetLastWriteTime(Path.Combine(modelFilePath, "RDTBindings.py")) > File.GetLastWriteTime(outPath)) {
-                    runInference = true;
-                    Trace.WriteLine("Regenerating model...");
-                    var processInfo = new ProcessStartInfo {
-                        FileName = "python3",
-                        Arguments = $"\"{fromPython}\"",
-                        RedirectStandardOutput = true,
-                        UseShellExecute = false,
-                        CreateNoWindow = true,
-                        WorkingDirectory = modelFilePath
-                    };
-                    using var process = Process.Start(processInfo);
-                    Debug.Assert(process != null, "Process failed to start.");
-                    StreamReader reader = process.StandardOutput;
-                    string output = reader.ReadToEnd();
-                    File.WriteAllText(outPath, output);
-                    process.WaitForExit();
-                    Assert.Equal(0, process.ExitCode);
-                }
+        private static void GenerateFromPython(string fromPython, string model, string executingAssemblyPath) {
+            var modelDirPath = Path.Combine(executingAssemblyPath, "models-and-rules");
+            var outPath = Path.Combine(modelDirPath, model);
+            outPath = Path.GetFullPath(outPath);
+            Assert.True(File.Exists(Path.Combine(modelDirPath, fromPython)));
+            Assert.True(File.Exists(Path.Combine(modelDirPath, "RDTBindings.py")));
+            if (runInference || !File.Exists(outPath) || File.GetLastWriteTime(Path.Combine(modelDirPath, fromPython)) > File.GetLastWriteTime(outPath) || File.GetLastWriteTime(Path.Combine(modelDirPath, "RDTBindings.py")) > File.GetLastWriteTime(outPath)) {
+                runInference = true;
+                Trace.WriteLine("Regenerating model...");
+                var processInfo = new ProcessStartInfo {
+                    FileName = "python3",
+                    Arguments = Path.Combine(modelDirPath, fromPython),
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                using var process = Process.Start(processInfo);
+                Debug.Assert(process != null, "Process failed to start.");
+                StreamReader reader = process.StandardOutput;
+                string output = reader.ReadToEnd();
+                File.WriteAllText(outPath, output);
+                process.WaitForExit();
+                Assert.Equal(0, process.ExitCode);
             }
         }
 
@@ -253,18 +240,21 @@ namespace TestProject
 
             public class AMQSensor(string sensorName, string procedureName, Func<IncubatorFields, double> f) : ISensor
             {
+                private bool _onceOnly = true;
+
                 public string SensorName { get; private init; } = sensorName;
 
                 public string ProcedureName { get; private init; } = procedureName;
 
                 public object ObservePropertyValue(params object[] inputProperties)
                 {
-                    // Console.WriteLine("Observing AMQ Sensor Value: " + inputProperties[0]);
+                    Assert.True(_onceOnly, "Really just expecting it to be called once here.");
                     IncubatorFields? myData = null;
                     Monitor.Enter(i);
                     myData = i.Data;
                     Monitor.Exit(i);
                     Debug.Assert(myData != null, "No data received from Incubator AMQP.");
+                    _onceOnly = false;
                     return f(myData);
                 }
             }

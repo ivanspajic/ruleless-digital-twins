ebf26d2faf8db25dd066d0e347d41b55c84b4823
Volker Stolz <vsto@hvl.no>
2026-04-10 15:04:41 +0000

    Latest round of evaluation/simulation tweaks.
    Pull in a package which allows us to include the current git hash when "logging" results to, as in:
    ```
     tail -2 SmartNode/TestProject/bin/Debug/net8.0/plan_times.txt
    1772bcc-DIRTY: M370-instance.ttl,2,40,106.488,True,7.3091707
    1772bcc-DIRTY: M370-instance.ttl,4,5536,114.696,False,25.0128886
    ```

--- SmartNode/TestProject/MapekTests.cs
+++ SmartNode/TestProject/MapekTests.cs
@@ -1,8 +1,11 @@
 using Fitness;
+using Logic.CaseRepository;
 using Logic.FactoryInterface;
 using Logic.Mapek;
+using Logic.Models.DatabaseModels;
 using Logic.Models.MapekModels;
 using Logic.Models.OntologicalModels;
+using MongoDB.Driver;
 using System.Diagnostics;
 using System.Reflection;
 using TestProject.Mocks.ServiceMocks;
@@ -12,13 +15,17 @@ namespace TestProject
     public class MapekTests
     {
         [Theory]
-        [InlineData("instance-model-1.ttl", "inferred-model-1.ttl",2,40,0)]
-        [InlineData("M370-instance.ttl", "M370-inferred.ttl",1,4,115.05600000000001)]
-        [InlineData("M370-instance.ttl", "M370-inferred.ttl",2,40,106.488)]
-        public async Task TestMapeK(String instance, String inferred, int rounds, int count, double minCost)
+        [InlineData("instance-model-1.ttl", "inferred-model-1.ttl",2,40,0, false, false)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl",1,4,115.05600000000001, false, false)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl", 2, 40, 106.488, true, false)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl", 4, 5536, 114.696, false, true)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl", 4, 5536, 114.696, true, true)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl", 8, 0, 0, false, true)]
+        [InlineData("M370-instance.ttl", "M370-inferred.ttl", 8, 0, 0, true, true)]
+        public async Task TestMapeK(String instance, String inferred, int rounds, int count, double minCost, bool useCase, bool nullLogger)
         {
             // Arrange
-            var serviceProvider = new ServiceProviderMock();
+            IRDTServiceProvider serviceProvider = nullLogger ? new NullServiceProviderMock() : new ServiceProviderMock();
 
             var rootDirectory = Directory.GetParent(Assembly.GetExecutingAssembly().Location)!.Parent!.Parent!.Parent!.Parent!.Parent!.FullName;
             var filepathArguments = new FilepathArguments
@@ -43,7 +50,7 @@ namespace TestProject
                 CycleDurationSeconds = 3600,
                 SleepyTimeMilliseconds = 0,
                 StartInReactiveMode = false,
-                UseCaseBasedFunctionality = false,
+                UseCaseBasedFunctionality = useCase,
                 UseDecisionLagMitigation = false, // XXX not supported for multiple FMUs yet.
                 UseEuclid = false
             };
@@ -58,13 +65,20 @@ namespace TestProject
             IMapekMonitor monitor = new MapekMonitor(serviceProvider);
             serviceProvider.Add(monitor);
 
+            if (useCase) {
+                var db = new DatabaseSettings() {ConnectionString = "mongodb://172.22.0.2:27017", DatabaseName = "testdb", CollectionName = "cases"};
+                serviceProvider.Add(db);
+                IMongoClient imc = new MongoClient(db.ConnectionString);
+                serviceProvider.Add(imc);
+                serviceProvider.Add(new CaseRepository(serviceProvider));
+            }
+
             // Test property we want to accumulate:
             var f_energy = new FProp("http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#EnergyConsumption");
             var f_temp = new FProp("http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#price");
             var f_prod = new FBinOpArith(f_energy, f_temp, (x, y) => x * y, name: "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#EnergyTimesPrice");
             var f_prod_acc = new FAcc<double>(f_prod, name: "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#AccumulatedEnergyTimesPrice");
 
-            // IMapekPlan plan = new MapekPlan(serviceProvider);
             IMapekPlan plan = new MMK(serviceProvider, new FOp[] { f_prod_acc });
             serviceProvider.Add(plan);
 
@@ -76,10 +90,15 @@ namespace TestProject
                     cache.PropertyCache.Properties.Add("http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#price",
                         new Property { Name = "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#price", Value = 0.0, OwlType = "http://www.w3.org/2001/XMLSchema#double" });
                 }
+
+                Stopwatch sw = Stopwatch.StartNew();
                 var simulationPathAndTree = await plan.Plan(cache);
+                sw.Stop();
+                Debug.WriteLine($"Planning took {sw.Elapsed.TotalSeconds} seconds total.");
+                using (StreamWriter f_out = File.AppendText("plan_times.txt")) { f_out.WriteLine($"{ThisAssembly.Git.Commit}{(ThisAssembly.Git.IsDirty ? "-DIRTY" : "")}: {instance},{rounds},{count},{minCost},{useCase},{sw.Elapsed.TotalSeconds}"); }
+
                 var path = simulationPathAndTree.Item2.Simulations;
                 var last = path.Last().PropertyCache.Properties["http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#AccumulatedEnergyTimesPrice"].Value;
-                Debug.WriteLine("Overall NOK: " + last);
 
                 // We compute the same value in different ways just to be sure:
                 var f_prod2 = new FBinOpArith(f_energy, f_temp, (x, y) => x * y, name: "http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#EnergyTimesPriceB");
@@ -106,8 +125,13 @@ namespace TestProject
 
                 // Check that all best paths are as they should be:
                 var paths = plan.GetOptimalSimulationPath(cache, simulationPathAndTree.Item1.SimulationPaths);
+                double least = (double)paths.Min(s => s.Simulations.Last().PropertyCache.Properties["http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#AccumulatedEnergyTimesPrice"].Value);
+                var leasts = paths.Where(s => (double)s.Simulations.Last().PropertyCache.Properties["http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#AccumulatedEnergyTimesPrice"].Value == least);
+                Debug.WriteLine(string.Join(",\n", leasts.First().Simulations.Select(s => string.Join(" + ", s.Actions.Select(a => a.Name)))));
+                
                 Assert.Equal(count, paths.Count());
-                Assert.Equal(minCost, paths.Min(s => s.Simulations.Last().PropertyCache.Properties["http://www.semanticweb.org/ivans/ontologies/2025/instance-model-1#AccumulatedEnergyTimesPrice"].Value));
+                Assert.Equal(minCost, least);
+                Assert.Single(leasts); // Doesn't have to be true, but is.
             }
             catch (Exception exception)
             {
